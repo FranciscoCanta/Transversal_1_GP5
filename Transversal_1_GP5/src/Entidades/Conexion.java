f7ad65e1d5cd7b76a1bb60cc6663f4f757b23934
Fernanda Arrieta <99426887+fernadaArrieta@users.noreply.github.com>
2024-10-03 16:36:54 -0300

    conexion

--- Transversal_1_GP5/src/Entidades/Conexion.java
+++ Transversal_1_GP5/src/Entidades/Conexion.java
@@ -1,7 +1,29 @@
 
 package Entidades;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
 
 public class Conexion {
+    public static void main(String[] args){
 
+      try {
+            //cargar driver de conexion
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            //coneccion al db
+            
+          Connection conec =  DriverManager.getConnection("jdbc:mariadb://localhost:3306/gp5_universidad","root", "");
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog( null, "Debe agregar los drivers al proyecto");
+                    
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog( null, "No se establecio la conecion a la DB");
+        }
+}
 }
--- Transversal_1_GP5/src/Vistas/Test_de_consola.java
+++ Transversal_1_GP5/src/Vistas/Test_de_consola.java
@@ -2,8 +2,8 @@ package Vistas;
 
 
 public class Test_de_consola {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
+     
         
     }
 
